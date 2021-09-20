bc7cf3b9837cc08e2e1a18b8648681f1b800c73c
Natalya Smirnova <natalya.smirnova@akvelon.com>
2021-09-20 18:45:48 +0300

    Initial commit, module 4

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const mount = (el) => {
+    ReactDOM.render(
+        <h1>hi</h1>,
+        el
+    );
+};
+
+if (process.env.NODE_ENV === 'development') {
+    const devRoot = document.querySelector('#_marketing-dev-root');
+    if (devRoot) {
+        mount(devRoot);
+    }
+}
+
+export { mount };
